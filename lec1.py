ba731fca95b0b19aa04395017d8e03da42b17cf6
Familkin <cepjil@mail.ru>
2022-12-14 22:02:41 +0300

    lec1

---
+++ lec1.py
@@ -0,0 +1,129 @@
+# print("Hello world")
+# value = None
+# a = 123
+# b = 1.23
+# c = 123
+
+# # print (type(a))
+# # print (type(b))
+# # print (type(value))
+# # s = 'hello \nworld'
+# # print(s)
+
+# print(a, '-', b, '-', c, '-', c)
+# print('{1}-{3}-{2}-{0}'.format(a, b, c, c))
+# print(f'-{a}-{b}-{c}-{c}')
+
+# f = True
+# print(f)
+
+# list = [1, 2, 3, 'hello', 1.23, True]
+
+
+# print('введите переменную a')
+# a = float (input())
+# print('введите переменную b')
+# b = int (input())
+
+# print(f'{a}{b}')
+# print(a+b)
+
+# a = 1.31321321
+# b = 8
+# c = round(a * b, 5)
+# print(c)
+
+
+# a = 3
+# a=a+5
+# a *=5
+# print(a)
+
+
+# a = 1 > 3 and 5 < 2
+# print(a)
+
+# a = 1 == 2
+# print(a)
+
+# a = 1 != 2
+# print(a)
+
+# a = [1, 2]
+# b = [1, 3]
+# print(a == b)
+
+# a = 1 < 3 < 5 < 6
+# print(a)
+
+# func =1
+# T=4
+# x=123
+# print(func<T>x)
+
+
+# f = 1 > 2 or 4 < 6
+# print(f)
+
+# f = [1, 2, 3, 4]
+# print(f)
+# print(not 2 in f)
+
+# is_odd = f[0] % 2 == 0
+# print(is_odd)
+
+# a = int(input("a="))
+# b = int(input('b='))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+# username = input('Введите имя:')
+# if username == "Маша":
+#     print("Привет Маша")
+# elif username == "Сережа":
+#     print("Привет Сережа")
+# elif username == "Олег":
+#     print("Привет Олег")
+
+# original = 123456789
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original//=10
+# print(inverted)
+
+
+# original = 123456789
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original//=10
+# else:
+#     print("первым")
+#     print("Выводится")
+# print(inverted)
+
+
+# for i in 1, 2, 3, 4, 5:
+#     print(i**2)
+
+
+# list = [1, 2, 3, 4, 5]
+# for i in list:
+#     print(i**2)
+
+
+# r = range(1,10,2)
+# for i in r:
+#     print(i)
+
+
+
+# r = 'qwerty'
+# for i in r:
+#     print(i)
+
+
+help(str)  
